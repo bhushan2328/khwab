4d08ade2cc8af7895dc73bb413387f86d0e30045
bushan4216 <bhushandigra7167@gmail.com>
2026-06-30 17:00:24 +0530

    added sherpa onnx dependency and created empty files

--- app/src/main/java/com/toblad/khwab/speech/SherpaManager.kt
+++ app/src/main/java/com/toblad/khwab/speech/SherpaManager.kt
@@ -1,4 +1,20 @@
 package com.toblad.khwab.speech
 
 class SherpaManager {
+
+    private val modelLoader = ModelLoader()
+    private val audioRecorder = AudioRecorder()
+    private val speechRecognizer = SpeechRecognizer()
+
+    fun initialize() {
+        modelLoader.loadModel()
+    }
+
+    fun startListening() {
+        audioRecorder.startRecording()
+    }
+
+    fun stopListening() {
+        audioRecorder.stopRecording()
+    }
 }
